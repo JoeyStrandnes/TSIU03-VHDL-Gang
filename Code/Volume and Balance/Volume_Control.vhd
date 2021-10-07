8a479f0d89bf5a7a73e4a9f801e11820446ab0de
Leczez <53293717+Leczez@users.noreply.github.com>
2021-10-07 12:08:50 +0200

    Nothing much has changed but I have a pretty clear picture of how I shall implement the volume control

--- Code/Volume and Balance/Volume_Control.vhd
+++ Code/Volume and Balance/Volume_Control.vhd
@@ -17,25 +17,10 @@ end entity;
 
 
 architecture Volume of Volume_Control is
-	signal volume : signed(15 downto 0) := (others => 0);
-	signal counter : unsigned(3 downto 0) := "0000";
-begin
-
-	output <= input when control_setting = (others => 15) else
-				 input(15) & 3*input(14 downto 0) when control_setting = (others => 14) else
-				 input(15) & "0" & 9*input(13 downto 0) when control_setting = (others => 13) else
-				 input(15) & "00" & 27*input(12 downto 0) when control_setting = (others => 12) else
-				 x when control_setting = (others => 11) else
-				 x when control_setting = (others => 10) else
-				 x when control_setting = (others => 9) else
-				 x when control_setting = (others => 8) else
-				 x when control_setting = (others => 7) else
-				 x when control_setting = (others => 6) else
-				 x when control_setting = (others => 5) else
-				 x when control_setting = (others => 4) else
-				 x when control_setting = (others => 3) else
-				 x when control_setting = (others => 2) else
-				 x when control_setting = (others => 1) else
-				 (others => 0) when control_setting = (others => 0) else (others => 0);
+	signal volume : signed(15 downto 0) := 0;
+	signal result : signed(32 downto 0);
+begin 
+				 
+	
 
 end architecture;
