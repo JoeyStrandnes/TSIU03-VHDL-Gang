5f8afa265b8a5945cffe0dd0bb6509d24160b645
Johan Olsson <johol842@student.liu.se>
2021-10-14 16:33:49 +0200

    Ändrat gain-värden lite

--- Code/BIG_SOUND_BLOCK/Dist.vhd
+++ Code/BIG_SOUND_BLOCK/Dist.vhd
@@ -14,47 +14,47 @@ architecture distArch of Dist is
 	signal sample_out_tmp : signed(15 downto 0);
 	signal mult_tmp: signed(31 downto 0);
 	signal gain: signed(15 downto 0);
-	constant N : integer := 5;
+	constant N : integer := 1;
 begin
 with distcontrol select
 
 		threshold <=   to_signed(32767, 16) when "00000", -- 32767*1/16 
-							to_signed(1024*N, 16) when "00001", -- 32767*2/16 
-							to_signed(512*N, 16) when "00010",	-- 32767*3/16 
-							to_signed(256*N, 16) when "00011", -- 32767*4/16 
-							to_signed(128*N, 16) when "00100",
-							to_signed(96*N, 16) when "00101",
-							to_signed(64*N, 16) when "00110",
-							to_signed(48*N, 16) when "00111",
-							to_signed(32*N, 16) when "01000",
-							to_signed(24*N, 16) when "01001",
-							to_signed(16*N, 16) when "01010",
-							to_signed(12*N, 16) when "01011",
+							to_signed(16834*N, 16) when "00001", -- 32767*2/16 
+							to_signed(8192*N, 16) when "00010",	-- 32767*3/16 
+							to_signed(4096*N, 16) when "00011", -- 32767*4/16 
+							to_signed(2048*N, 16) when "00100",
+							to_signed(1024*N, 16) when "00101",
+							to_signed(512*N, 16) when "00110",
+							to_signed(256*N, 16) when "00111",
+							to_signed(128*N, 16) when "01000",
+							to_signed(64*N, 16) when "01001",
+							to_signed(32*N, 16) when "01010",
+							to_signed(16*N, 16) when "01011",
 							to_signed(8*N, 16) when "01100",
 							to_signed(4*N, 16) when "01101",
 							to_signed(2*N, 16) when "01110",	-- 32767*14/16 
 							to_signed(1*N, 16)  when "01111", -- 32767*15/16 
 							to_signed(0, 16)  when others; 
 
-							
+					
 with distcontrol select
 		gain <=				to_signed(1, 16) when "00000",  
-								to_signed(2*N, 16) when "00001", 
-								to_signed(4*N, 16) when"00010",	 
-								to_signed(8*N, 16) when "00011", 
-								to_signed(16*N, 16) when "00100",
-								to_signed(24*N, 16) when "00101",
-								to_signed(32*N, 16) when "00110",
-								to_signed(48*N, 16) when "00111",
-								to_signed(64*N, 16) when "01000",
-								to_signed(96*N, 16) when "01001",
-								to_signed(128*N, 16) when "01010",
-								to_signed(188*N, 16) when "01011",
+								to_signed(1*N, 16) when "00001", 
+								to_signed(2*N, 16) when"00010",	 
+								to_signed(4*N, 16) when "00011", 
+								to_signed(4*N, 16) when "00100",
+								to_signed(8*N, 16) when "00101",
+								to_signed(8*N, 16) when "00110",
+								to_signed(16*N, 16) when "00111",
+								to_signed(16*N, 16) when "01000",
+								to_signed(32*N, 16) when "01001",
+								to_signed(64*N, 16) when "01010",
+								to_signed(128*N, 16) when "01011",
 								to_signed(256*N, 16) when "01100",
 								to_signed(512*N, 16) when "01101",
 								to_signed(1024*N, 16) when "01110",	 
 								to_signed(2048*N, 16)  when "01111",  
-								to_signed(4196*N, 16)  when others; -- 
+								to_signed(1*N, 16)  when others; 
 						
 	process (clk, rstn)
 	begin
@@ -64,8 +64,10 @@ with distcontrol select
 			
 				if sample_in > threshold and sample_in > 0 then -- Kollar inte 0
 					sample_out_tmp <= threshold;
+					
 				elsif sample_in < -threshold and sample_in < 0 then --Kollar inte 0
 					sample_out_tmp <= -threshold;
+					
 				else 
 					sample_out_tmp <= sample_in;
 				end if;
@@ -74,5 +76,6 @@ with distcontrol select
 	
 	mult_tmp <= sample_out_tmp * gain;
 	sample_out <= resize( mult_tmp, sample_out'length);
+	
 end architecture;
 
--- Code/Johans Tester/Quartus_proj/MSim/TB_SRAM.vhd
+++ Code/Johans Tester/Quartus_proj/MSim/TB_SRAM.vhd
@@ -14,18 +14,17 @@ entity TB_SRAM is
 end entity;
 
 architecture sim of TB_SRAM is
-  --signal data : unsigned(15 downto 0); 
   type RAM_1Mx16 is array (0 to N -1) of signed(15 downto 0);
   --signal RAM_DATA : RAM_1Mx16 := (others => (others=>'0'));
   
 begin
-	process(sram_addr, sram_data, sram_we, sram_ce) begin	
-		if sram_ce = '0' and sram_we = '0' then
-	--		RAM_DATA( to_integer(sram_addr) ) <= sram_data;
-		end if;
-	end process;
-	--sram_data <= RAM_DATA(to_integer(sram_addr)) when sram_ce = '0' and sram_oe = '0' else
-	--             (others=>'Z');
+--	process(sram_addr, sram_data, sram_we, sram_ce) begin	
+--		if sram_ce = '0' and sram_we = '0' then
+--		RAM_DATA( to_integer(sram_addr) ) <= sram_data;
+--		end if;
+--	end process;
+--		sram_data <= RAM_DATA(to_integer(sram_addr)) when sram_ce = '0' and sram_oe = '0' else
+--             (others=>'Z');
 
 -- SHARED VAR VERSION:
 	process(sram_addr, sram_data, sram_we, sram_ce, sram_oe)
